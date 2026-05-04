b1b71cd7f5bbcc6050800eb371246d3294f7a2c9
HSU,WEI-YU <41343121@nfu.edu.tw>
2026-05-04 19:55:46 +0800

    Update AOVandAOE.cpp

--- homework(2)-2/src/AOVandAOE.cpp
+++ homework(2)-2/src/AOVandAOE.cpp
@@ -2,7 +2,7 @@
 #include <vector>
 #include <queue>
 #include <algorithm>
-#include <stack>
+
 
 using namespace std;
 
@@ -66,35 +66,64 @@ public:
     }
 };
 
-struct AOEEdge {
-    int u, v, weight;
-};
-
 class AOE {
 private:
     int n;
-    vector<AOEEdge> edges;
+    vector<vector<pair<int, int>>> adj;
+    vector<int> inD;
 
 public:
-    AOE(int vertices) : n(vertices) {}
+    AOE(int vertices) : n(vertices), adj(vertices), inD(vertices, 0) {}
 
     void AddEdge(int u, int v, int weight) {
-        edges.push_back({ u, v, weight });
+        if (u < 0 || u >= n || v < 0 || v >= n) return;
+        adj[u].push_back(make_pair(v, weight));
+        inD[v]++;
     }
 
     void Time() {
-        vector<int> earliest(n, 0);
+        queue<int> q;
+        vector<int> indeg = inD;
 
-        for (const auto& edge : edges) {
-            if (earliest[edge.u] + edge.weight > earliest[edge.v]) {
-                earliest[edge.v] = earliest[edge.u] + edge.weight;
+        for (int i = 0; i < n; i++) {
+            if (indeg[i] == 0) q.push(i);
+        }
+
+        vector<int> topo;
+        while (!q.empty()) {
+            int u = q.front();
+            q.pop();
+            topo.push_back(u);
+
+            for (const auto& edge : adj[u]) {
+                int v = edge.first;
+                indeg[v]--;
+                if (indeg[v] == 0) q.push(v);
             }
         }
 
         cout << "\n--- AOE Network: Earliest Time ---" << endl;
+
+        if ((int)topo.size() != n) {
+            cout << "錯誤：AOE 網路中包含迴圈（非 DAG），無法計算 ve！" << endl;
+            return;
+        }
+
+        vector<int> earliest(n, 0);
+        for (int u : topo) {
+            for (const auto& edge : adj[u]) {
+                int v = edge.first;
+                int w = edge.second;
+                earliest[v] = max(earliest[v], earliest[u] + w);
+            }
+        }
+
+        int projectFinish = 0;
         for (int i = 0; i < n; i++) {
             cout << "事件 " << i << " 的最早發生時間: " << earliest[i] << endl;
+            projectFinish = max(projectFinish, earliest[i]);
         }
+        cout << "專案最早完工時間: " << projectFinish << endl;
     }
 };
 
